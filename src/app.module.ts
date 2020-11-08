e3eed08e2d1917cee2810b91a564e8568d2ed0fe
Alex Komar <909komar@gmail.com>
2020-11-08 20:10:47 +0200

    add user and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'photo_booth',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migration/*.js'],
+      cli: {
+        migrationsDir: 'migration',
+      },
+      synchronize: true,
+    }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import * as dotenv from "dotenv";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+dotenv.config();
+
+
+const logger = new Logger()
+
+const port = process.env.PORT || 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  logger.log(`Server was started at port ${port}`)
 }
+
 bootstrap();
